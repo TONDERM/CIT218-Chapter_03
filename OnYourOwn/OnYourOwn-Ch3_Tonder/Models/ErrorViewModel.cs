1935b71d27743ff5c7b8810d8a4a54cbf1e74cab
TONDERM <tonderm@mail.nmc.edu>
2022-09-30 13:26:38 -0400

    Add project files.

---
+++ OnYourOwn/OnYourOwn-Ch3_Tonder/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace OnYourOwn_Ch3_Tonder.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
